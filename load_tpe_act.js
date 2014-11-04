0cf22742b8aa945d24708683b13a8df58f889799
alikefool <aliku@yahoo-inc.com>
2014-11-04 11:39:16 +0800

    fix & simplize callback func

--- load_tpe_act.js
+++ load_tpe_act.js
@@ -1,82 +1,47 @@
-//var new_events = {};
-//var new_event = {};
-//var BreakException= {};
 var request = require('request');
 
-var getLv2 = function(x, topicId, callback) {
-	//var events;
-	request('http://www.api.cloud.taipei.gov.tw/CSCP_API/pnc/cei/rows/'+topicId, 
-			function(error, response, body){
-		if (!error && response.statusCode == 200) {
-			callback(x, JSON.parse(body));
-		}
-	});
-	//return events;
-}
-
-var getLv1 = function(x, cat_id, callback) {
-	//var 
-	//all_events = {};
-	request('http://www.api.cloud.taipei.gov.tw/CSCP_API/pnc/cei/categories/'+cat_id+'/topics', 
+var getLv1 = function(xargs, callback) {
+	request('http://www.api.cloud.taipei.gov.tw/CSCP_API/pnc/cei/categories/' + xargs.cat_id + '/topics' , 
 			function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			callback(x, JSON.parse(body));
+			callback(xargs, JSON.parse(body));
 			//console.log(events);
-			/*
-			for (var i = 0; i < events.length; i++) {
-				topic_events = {};
-				var callbackLv2 = function(topic_events) {
-					for (var j = 0; j < topic_events.length; j++) {
-						all_event = topic_events[j];
-						all_event.topicId= events[i].topicId;
-						all_event.activityImg= events[i].activityImg;
-						all_event.catId= cat_id;
-						topic_events.push(all_event);
-					}
-				}
-
-				getLv2(events[i].topicId, callbackLv2);
-
-			}*/
 		}
 	})
 }
 
-/*
-for (var cat_id = 1; cat_id <= 2; cat_id++) {
-
-}
-*/
-var new_events = {} ;
-getLv1(new_events, 1, function(args, events){
-	//console.log(events);
-	for (var i = 0; i < events.length; i++) {
-		//var x = [];
-		getLv2(events, events[i].topicId, function(eventsL1, eventsL2){
-			console.log(eventsL2);
-			//console.log(eventsL1);
-		});
-		//console.log(x);
-	}
-	/*
-	var callbackLv2;
-	for (var i = 0; i < events.length; i++) {
-		topic_events = {};
-		callbackLv2 = function(topic_events) {
-			for (var j = 0; j < topic_events.length; j++) {
-				all_event = topic_events[j];
-				all_event.topicId= events[i].topicId;
-				all_event.activityImg= events[i].activityImg;
-				all_event.catId= cat_id;
-				topic_events.push(all_event);
-			}
+var getLv2 = function(xargs, callback) {
+	var eventL1 = xargs.eventL1;
+	request('http://www.api.cloud.taipei.gov.tw/CSCP_API/pnc/cei/rows/'+ eventL1.topicId , 
+			function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			callback(xargs, JSON.parse(body));
 		}
-		getLv2(events[i].topicId, callbackLv2);
+	});
+}
 
+for (var cat_id = 1; cat_id <= 1; cat_id++) {
+getLv1({'cat_id': cat_id}, function(args, eventsL1){
+	//console.log(args.cat_id);
+	for (var i = 0; i < eventsL1.length; i++) {
+		//var x = [];
+		getLv2({'eventL1': eventsL1[i], 'cat_id': args.cat_id}, function(args, eventsL2){
+			//console.log(eventsL2);
+			//console.log(args);
+			catId = args.cat_id;
+			eventL1 = args.eventL1;
+			for (var j = 0; j < eventsL2.length; j++) {
+				all_event = eventsL2[j];
+				all_event.topicId= eventL1.topicId;
+				all_event.activityImg= eventL1.activityImg;
+				all_event.catId= catId;
+				//topic_events.push(all_event);
+				console.log(all_event.catId + ' : ' + all_event.topicId + ' : ' +all_event.webPageTitle);
+			}
+		});
 	}
-	*/
 });
-//console.log(new_events);
+}
 
 /*
 new_events.forEach(function(event, index, array){
